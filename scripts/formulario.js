6191683e10e89107f6ac0cd7380f1e975bbdae94
LsiedsC <lucas_correa14@estudante.sesisenai.org.br>
2026-04-08 12:56:18 -0300

    add - Adicionei mais algumas coisas

--- scripts/formulario.js
+++ scripts/formulario.js
@@ -2,21 +2,93 @@ document.getElementById("formAdocao").addEventListener("submit", function (e) {
   e.preventDefault();
 
   let nome = document.getElementById("nome").value;
-    if(nome.length < 3) return alert("Nome Inválido");
-
   let email = document.getElementById("email").value;
   let telefone = document.getElementById("telefone").value;
-    if(telefone.length < 9 ) return alert("Nome Inválido");
   let cpf = document.getElementById("cpf").value;
   let idade = document.getElementById("idade").value;
   let cidade = document.getElementById("cidade").value;
   let moradia = document.getElementById("moradia").value;
   let quintal = document.querySelector('input[name="quintal"]:checked');
   let pets = document.querySelector('input[name="pets"]:checked');
-  let horas = document.getElementById("pets").value;
+  let horas = document.getElementById("horas").value;
   let motivos = document.getElementById("motivo").value;
-  let aceito = document.getElementById("aceito").value;
+  let aceito = document.getElementById("aceito");
 
-  document.getElementById("resultado").innerHTML = "Cadastro realizado com sucesso!<br>" + "Nome: " + nome;
+
+
+  //Regras de validação
+
+  //Nome//
+  if (nome ===""){
+    return alert("Digite um nome!")
+  } else if (nome.length < 3) return alert("Nome Inválido");
+
+  //Email//
+  if (!email.includes("@")) return alert("Email inválido: O email deve conter @");
   
+  //Telefone//
+  if (telefone === ""){
+    return alert("Digite um telefone!")
+  } else if (telefone.length < 8 || telefone.length > 11) return alert("Telefone Inválido");
+
+  //CPF//
+  if (!cpf === "") return alert("Digite um CPF");
+
+  //Idade//
+  if (idade ===""){
+    return alert("Digite uma idade!");
+  }else if (idade < 18) {
+    return alert("Idade não permitida para adotar um pet.");
+  }
+
+  //Cidade//
+  if(cidade ===""){
+    return alert("Digite uma cidade!");
+  }
+
+  //Moradia//
+  if(moradia ===""){
+    return alert("Escolha um tipo de moradia!");
+  }
+
+  if(moradia === "Apartamento"){
+    return prompt("O apartamento permite animais?");
+  }
+
+  if(moradia ==="Casa"){
+    return prompt("Seu quintal é seguro?");
+  }
+
+  //Quintal//
+  if(!quintal){
+    return alert("Escolha se você tem ou não um quintal.");
+  }
+  //Pets//
+  if(!pets){
+    return alert("Escolha se você já teve ou nunca teve um pet.");
+  }
+
+  if(pets.value === "nao"){
+    return alert("Tudo bem, você recebera acompanhamento da ONG LSC!");
+  }
+
+  //Horas//
+  if(horas < 0 || horas > 24){
+    return alert("Informe uma quantidade de horas válida.");
+  } else if (horas > 8) {
+    return alert("Você deve contratar uma baba para cuidar do seu cachorro caso queira adota-lo ainda.");
+  }
+    
+  //Horas//
+  if(horas > 24 || horas < 0) return alert("Insira um horario válido!");
+
+  //Motivos//
+  if(motivos.length < 10) return alert("Seu motivo deve ter mais que 10 caracteres.");
+
+  //Termo//
+  if(!aceito.checked) {
+    alert ("Você precisa aceitar os termos para continuar.");
+    return;
+  }
+
 });
